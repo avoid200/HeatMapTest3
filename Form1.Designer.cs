55020024ed9a502ee238d4df70268bd9fa4e82a1
Daniel Bischoff <avoid@arcor.de>
2020-04-24 15:54:11 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace HeatMapTest3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.trackBar_transparenz = new System.Windows.Forms.TrackBar();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar_transparenz)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(530, 386);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.addWert);
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel2.BackgroundImage = global::HeatMapTest3.Properties.Resources.Grundriss400;
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(536, 392);
+			this.panel2.TabIndex = 2;
+			// 
+			// trackBar_transparenz
+			// 
+			this.trackBar_transparenz.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.trackBar_transparenz.Location = new System.Drawing.Point(554, 15);
+			this.trackBar_transparenz.Maximum = 255;
+			this.trackBar_transparenz.Name = "trackBar_transparenz";
+			this.trackBar_transparenz.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar_transparenz.Size = new System.Drawing.Size(56, 389);
+			this.trackBar_transparenz.TabIndex = 3;
+			this.trackBar_transparenz.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+			this.trackBar_transparenz.Value = 100;
+			this.trackBar_transparenz.ValueChanged += new System.EventHandler(this.trackBar_transparenz_ValueChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(622, 416);
+			this.Controls.Add(this.trackBar_transparenz);
+			this.Controls.Add(this.panel2);
+			this.Name = "Form1";
+			this.Text = "HeatMap Test 3";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar_transparenz)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TrackBar trackBar_transparenz;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;     // für PathGradientBrush und ColorBlend
+using System.Drawing.Imaging;       // für PixelFormat
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeatMapTest3
+{
+	public partial class Form1 : Form
+	{
+
+		List<Werte> wertListe = new List<Werte>();																																	// erstellt leere Liste
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void addWert(object sender, MouseEventArgs e)
+		{
+      wertListe.Add(new Werte() { Wert = 80, X = e.X, Y = e.Y });
+      updateBitmap();
+		}                                                    // erzeugt eine Liste mit werten für eine NoiceMap.
+
+		private void updateBitmap()
+		{
+			Bitmap bitMap = new Bitmap(pictureBox1.Width, pictureBox1.Height);																				// erstellt leere Bitmap
+			bitMap = CreateIntensityMask(bitMap, wertListe);                                                          // füllt Bitmap mit Werten
+      pictureBox1.Image = Colorize(bitMap, (byte)trackBar_transparenz.Value);                                   // färbt die Bitmap  incl. Transparenz
+      //pictureBox1.Image = bitMap;                                                                             // Maske ohne farbe und transparenz
+    }
+
+		private Bitmap CreateIntensityMask(Bitmap bitMapMask, List<Werte> wertListeList)
+		{
+			Graphics DrawMask = Graphics.FromImage(bitMapMask);                                                       // erzeugt eine neue Maske
+			DrawMask.Clear(Color.White);                                                                              // füllt die neue Maske mit einer Farbe
+			foreach (Werte DataPoint in wertListeList)                                                                // geht die liste mit werten durch
+			{
+        DrawWerte(DrawMask, DataPoint);                                                             // fügt der maske die werte aus der liste hinzu
+			}
+			return bitMapMask;                                                                                        // gibt die ausgefüllte maske zurück
+		}                         // erstellt eine Maske(NoiceMap) der Werte und gibt diese zurück
+
+    private void DrawWerte(Graphics Canvas, Werte Wert)
+    {
+      List<Point> CircumferencePointsList = new List<Point>();
+      Point CircumferencePoint;
+      Point[] CircumferencePointsArray;
+      float fRatio = 1F / Byte.MaxValue;
+      byte bHalf = Byte.MaxValue / 2;
+      int iIntensity = (byte)(Wert.Wert - ((Wert.Wert - bHalf) * 2));
+      float fIntensity = iIntensity * fRatio;
+      for (double i = 0; i <= 360; i += 10)
+      {
+        CircumferencePoint = new Point();
+        CircumferencePoint.X = Convert.ToInt32(Wert.X + Wert.Wert * Math.Cos(ConvertDegreesToRadians(i))); // Wert.Wert = früher Radius
+        CircumferencePoint.Y = Convert.ToInt32(Wert.Y + Wert.Wert * Math.Sin(ConvertDegreesToRadians(i))); // Wert.Wert = früher Radius
+        CircumferencePointsList.Add(CircumferencePoint);
+      }
+      CircumferencePointsArray = CircumferencePointsList.ToArray();
+      PathGradientBrush GradientShaper = new PathGradientBrush(CircumferencePointsArray);
+      ColorBlend GradientSpecifications = new ColorBlend(4);
+      GradientSpecifications.Positions = new float[4] { 0, fIntensity, fIntensity, 1 };
+      if ((Wert.Wert / 50) < 0)
+      {
+        if ((Wert.Wert / 25) < 0)
+        {
+          GradientSpecifications.Colors = new Color[4] {
+                        Color.FromArgb(0, Color.White),
+                        Color.FromArgb(Wert.Wert/4, Color.LightGray),
+                        Color.FromArgb(Wert.Wert/2, Color.DarkGray),
+                        Color.FromArgb(Wert.Wert*2, Color.Black)
+                    };
+        }
+        else
+        {
+          GradientSpecifications.Colors = new Color[4] {
+                        Color.FromArgb(0, Color.White),
+                        Color.FromArgb(Wert.Wert/25, Color.LightGray),
+                        Color.FromArgb(Wert.Wert/9, Color.DarkGray),
+                        Color.FromArgb(Wert.Wert*2, Color.Black)
+                    };
+        }
+      }
+      else
+      {
+        GradientSpecifications.Colors = new Color[4] {
+                    Color.FromArgb(0, Color.White),
+                    Color.FromArgb(Wert.Wert/50, Color.LightGray),
+                    Color.FromArgb(Wert.Wert/25, Color.DarkGray),
+                    Color.FromArgb(Wert.Wert*2, Color.Black)
+                };
+      }
+      GradientShaper.InterpolationColors = GradientSpecifications;
+      Canvas.FillPolygon(GradientShaper, CircumferencePointsArray);
+    }                                                      // fügt der Maske(NoiceMap) einen Wert hinzu
+
+    private double ConvertDegreesToRadians(double degrees)
+    {
+      double radians = (Math.PI / 180) * degrees;
+      return (radians);
+    }                                                   // rechnet Grad in Bogenmaß um
+
+    public static Bitmap Colorize(Bitmap Mask, byte Alpha)
+    {
+      Bitmap Output = new Bitmap(Mask.Width, Mask.Height, PixelFormat.Format32bppArgb);
+      Graphics Surface = Graphics.FromImage(Output);
+      Surface.Clear(Color.Transparent);
+      ColorMap[] Colors = CreatePaletteIndex(Alpha);
+      ImageAttributes Remapper = new ImageAttributes();
+      Remapper.SetRemapTable(Colors);
+      Surface.DrawImage(Mask, new Rectangle(0, 0, Mask.Width, Mask.Height), 0, 0, Mask.Width, Mask.Height, GraphicsUnit.Pixel, Remapper);
+      return Output;
+    }
+
+    private static ColorMap[] CreatePaletteIndex(byte Alpha)
+    {
+      ColorMap[] OutputMap = new ColorMap[256];
+      Bitmap Palette = Properties.Resources.intensity_mask2;                                                    // intensity_mask2.bmp als Resources
+      for (int X = 0; X <= 255; X++)
+      {
+        OutputMap[X] = new ColorMap();
+        OutputMap[X].OldColor = Color.FromArgb(X, X, X);
+        OutputMap[X].NewColor = Color.FromArgb(Alpha, Palette.GetPixel(X, 0));
+      }
+      return OutputMap;
+    }
+
+    private void trackBar_transparenz_ValueChanged(object sender, EventArgs e)
+    {
+      updateBitmap();
+    }
+  }
+  public struct Werte
+	{
+		public int Wert { get; set; }
+		public float X { get; set; }
+		public float Y { get; set; }
+	}																																											  // list Objekt für die Messwerte.
+}
